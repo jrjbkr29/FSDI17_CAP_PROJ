99e5899112c832a135e936edcab1379e7f8c5354
Johnny Jimenez <johnny.robert.jimenez@gmail.com>
2021-07-29 22:17:10 -0700

    added package.json, views, settings, and urls

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tasks',
     'rest_framework',
+    'frontend',
 ]
 
 MIDDLEWARE = [
--- taskmanager/taskmanager/urls.py
+++ taskmanager/taskmanager/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
     path('', include('tasks.urls')),
+    path('admin/', admin.site.urls),
 ]
